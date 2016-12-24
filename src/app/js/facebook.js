ab748475f62a421f0be4190c6f02236e1ae91a67
Rico <rpeinado75@gmail.com>
2016-12-23 23:44:40 -0800

    New facebook API js

---
+++ src/app/js/facebook.js
@@ -0,0 +1,24 @@
+/**
+ * Created by rpeinado on 12/23/16.
+ */
+/* Facebook API to intergrate sharing on the website */
+$(function() {
+
+  window.fbAsyncInit = function() {
+    FB.init({
+      appId      : 'your-app-id',
+      xfbml      : true,
+      version    : 'v2.8'
+    });
+    FB.AppEvents.logPageView();
+  };
+
+  (function(d, s, id){
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) {return;}
+    js = d.createElement(s); js.id = id;
+    js.src = "//connect.facebook.net/en_US/sdk.js";
+    fjs.parentNode.insertBefore(js, fjs);
+  }(document, 'script', 'facebook-jssdk'));
+
+});
