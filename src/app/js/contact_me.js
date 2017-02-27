eded46af40f85785d7646e352c60f1aed5e94711
Ricardo Peinado <rpeinado75@gmail.com>
2017-02-26 18:16:17 -0800

    Update mail object link

--- src/app/js/contact_me.js
+++ src/app/js/contact_me.js
@@ -18,7 +18,7 @@ $(function() {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
             $.ajax({
-                url: "././API/mail/contact_me.php",
+                url: "././app/mail/contact_me.php",
                 type: "POST",
                 data: {
                     name: name,
